a93a0e24a860b92e241bfaf63f33e73abf9c22a8
Cedzi-95 <cedricmasinzo@yahoo.com>
2025-11-19 17:09:30 +0100

    Add implementation ofEfRepository for all entities to share common CRUD operations

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 
-public interface IRepository<T>
+public interface IRepository<T> where T : class
 {
     public  Task AddAsync(T entity);
     public  Task<IEnumerable<T>> GetAllAsync();
-    public Task<T?> GetByIdAsync(Guid Id);
+    public Task<T?> GetByIdAsync(Guid id);
     public Task UpdateAsync(T entity);
     public Task DeleteAsync(T entity);
 
---
+++ Repositories/EfRepository.cs
@@ -0,0 +1,39 @@
+
+using Microsoft.EntityFrameworkCore;
+
+public class EfRepository<T> : IRepository<T> where T : class
+{
+    private readonly  AppDbContext _context;
+    public EfRepository(AppDbContext context)
+    {
+        _context = context;
+
+    }
+    public async Task AddAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+         _context.Set<T>().Remove(entity);
+         await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+       _context.Set<T>().Update(entity);
+       await _context.SaveChangesAsync();
+    }
+}
