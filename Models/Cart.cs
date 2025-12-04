53c09e8680293ab89f3b699b55785db7a69542e0
Cedzi-95 <cedricmasinzo@yahoo.com>
2025-12-04 17:08:29 +0100

    Add Cart and CartItem models

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -1,9 +1,17 @@
 public class Cart
 {
     public Guid Id { get; set; }
-    public Guid Customer { get; set; }
-    public UserEntity? UserEntity { get; set; }
-  
+    public Guid UserId { get; set; }
+    public UserEntity? User { get; set; }
+    public ICollection<CartItem>? CartItems { get; set;}
 
+}
 
+public class CartItem
+{
+    public Guid ProductId { get; set; }
+    public Product? Product { get; set; }
+    public Guid CartId { get; set; }
+    public Cart? Cart { get; set;}
+    public int Quantity { get; set; }
 }
