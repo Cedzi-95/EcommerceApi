d83237a39d535bdeb4f0475493e14128fb4bd06f
Cedzi-95 <cedricmasinzo@yahoo.com>
2026-01-04 09:57:28 +0100

    Added new methods for paymentStatus and OrderStatus query operations in the order repository

--- Interfaces/IOrder.cs
+++ Interfaces/IOrder.cs
@@ -1,6 +1,8 @@
 public interface IOrderRepository : IRepository<Order>
 {
-    
+    Task<IEnumerable<Order>> GetOrdersByUserAsync(Guid userId);
+    Task OrderStatusAsync(Guid orderId, Status newOrderStatus);
+    Task PaymentStatusAsync(Guid OrderId, PaymentStatus newPaymentStatus);
 }
 
 public interface IOrderService
@@ -9,4 +11,7 @@ public interface IOrderService
     Task<IEnumerable<Order>> GetAllAsync();
     Task<Order> GetByIdAsyn(Guid orderId);
     Task<Order> UpdateAsync(Guid orderId);
+    Task<IEnumerable<Order>> GetOrdersByUserAsync(Guid userId);
+    Task<Order> OrderStatusAsync(Guid orderId);
+    Task<Order> PaymentStatusAsync(Guid OrderId);
 }
--- Repositories/OrderRepository.cs
+++ Repositories/OrderRepository.cs
@@ -1,7 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+
 public class OrderRepository : EfRepository<Order>, IOrderRepository
 {
     public OrderRepository(AppDbContext context) : base(context)
     {
+    }
+
+    public async Task<IEnumerable<Order>> GetOrdersByUserAsync(Guid userId)
+    {
+        var order = await _context.Orders
+        .Where(o => o.UserId == userId).ToListAsync();
+        return order;
+    }
+
+    public async Task OrderStatusAsync(Guid orderId, Status newOrderStatus)
+    {
+        var order = await _context.Orders
+        .Where(o => o.Id == orderId)
+        .FirstOrDefaultAsync();
+
+        if (order != null)
+        {
+            order.OrderStatus = newOrderStatus;
+        }
+        
+
+    }
+
+    public async Task PaymentStatusAsync(Guid OrderId, PaymentStatus NewPaymentStatus)
+    {
+       var order = await _context.Orders
+       .Where(o => o.Id == OrderId)
+       .FirstOrDefaultAsync();
+
+       if (order != null)
+        {
+            order.PaymentStatus = NewPaymentStatus;
+        }
+
     }
 }
 
